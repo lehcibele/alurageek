82e4e38e2e0b8040f70dcce7ca8f6b380f1d53ed
Leticia Cibele <leticiacibele.cl@gmail.com>
2025-05-09 08:47:21 -0300

    adicionado a função de criar card dinamicamente

--- src/js/index.js
+++ src/js/index.js
@@ -2,26 +2,63 @@ const inputNomeProduto = document.getElementById('nome-produto');
 const inputPrecoProduto = document.getElementById('preco-produto');
 const inputImagemProduto = document.getElementById('imagem-produto');
 const formulario = document.getElementById('forms-adicionar-produto');
+const containerListaProdutos = document.getElementById('container-lista-protudos');
+/*
+    Obtem os produtos salvos anteriormente no localStorage, convertendo o JSON de volta para um array com JSON.parse().
+    Se não houver nada salvo ainda (ou seja, o valor for null), usa um array vazio [].
+*/
+let produtos = JSON.parse(localStorage.getItem('produtos')) || [];
 
-const containerProdutos = document.getElementById('container-produtos');
-
-const listaProdutos = [];
+window.addEventListener('DOMContentLoaded', () => {
+    produtos.forEach(produto => criarCardProduto(produto));
+});
 
 formulario.addEventListener('submit', guardarProdutos);
 
 function guardarProdutos(event) {
     event.preventDefault();
 
+
+
     const nomeProduto = inputNomeProduto.value;
-    const precoProduto = inputPrecoProduto.value;
+    const precoProduto = parseFloat(inputPrecoProduto.value).toFixed(2);
     const imagemProduto = inputImagemProduto.value;
 
-    console.log(nomeProduto);
-    console.log(precoProduto);
-    console.log(imagemProduto);
+    const novoProduto = {
+        nomeProduto,
+        precoProduto,
+        imagemProduto
+    }
 
-    // função que cria o card
-    
+    produtos.push(novoProduto); // Adiciona o novo produto ao array produtos
 
+    // Converte o array atualizado para JSON com JSON.stringify() e salva no localStorage
+    localStorage.setItem('produtos', JSON.stringify(produtos)); 
 
+    // chama a função que cria o produto e exibi na tela
+    criarCardProduto(novoProduto);
+
+    formulario.reset(); // limpa o formulario
+}
+
+function criarCardProduto(produto) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    card.innerHTML = `
+        <div class="card-img">
+            <img src="${produto.imagemProduto}" alt="Game boy">
+        </div>
+
+        <h3 class="card-titulo-produto">${produto.nomeProduto}</h3>
+
+        <div class="card-container-conteudo">
+            <p class="card-preco-produto">${produto.precoProduto}</p>
+            <div class="card-icon-lixeira" id="card-icon-lixeira">
+                <img src="src/img/icon-lixeira.png" alt="Icone da lixeira">
+            </div>
+        </div>
+    `;
+
+    containerListaProdutos.appendChild(card);
 }
